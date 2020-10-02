7c82e360348f7c626ccd98855113b78e0b25538a
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-03 06:02:38 +0700

    #5 - Add passwordEncoder in SecurityConfiguration

--- src/main/java/com/hendisantika/springdocsample/config/SecurityConfiguration.java
+++ src/main/java/com/hendisantika/springdocsample/config/SecurityConfiguration.java
@@ -1,8 +1,12 @@
 package com.hendisantika.springdocsample.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +26,18 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder().encode("password"))
+                .authorities("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
