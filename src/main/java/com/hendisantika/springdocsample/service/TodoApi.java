ddc3a168af150ce94f2da6d0d5312b1ab334d7c7
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-03 06:07:40 +0700

    #11 - Add save service in TodoApi

--- src/main/java/com/hendisantika/springdocsample/service/TodoApi.java
+++ src/main/java/com/hendisantika/springdocsample/service/TodoApi.java
@@ -6,6 +6,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -31,4 +33,8 @@ public interface TodoApi {
     @GetMapping("/{id}")
     @ResponseStatus(code = HttpStatus.OK)
     Todo findById(@PathVariable String id);
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    Todo save(@RequestBody Todo todo);
 }
