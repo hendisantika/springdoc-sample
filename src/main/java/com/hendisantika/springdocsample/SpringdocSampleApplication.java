63795db9c53378b34b9500cf26df8627a68cd5db
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-03 05:53:32 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springdocsample/SpringdocSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springdocsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdocSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdocSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springdocsample/SpringdocSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springdocsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdocSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
