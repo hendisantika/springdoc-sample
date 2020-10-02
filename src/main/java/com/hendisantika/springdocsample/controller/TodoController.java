750ea6b470e2083b4d890e3966145ac1767a612e
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-03 06:09:26 +0700

    #14 - Add TodoController

---
+++ src/main/java/com/hendisantika/springdocsample/controller/TodoController.java
@@ -0,0 +1,17 @@
+package com.hendisantika.springdocsample.controller;
+
+import com.hendisantika.springdocsample.service.TodoApi;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springdoc-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/10/20
+ * Time: 06.08
+ */
+@RestController
+public class TodoController implements TodoApi {
+}
