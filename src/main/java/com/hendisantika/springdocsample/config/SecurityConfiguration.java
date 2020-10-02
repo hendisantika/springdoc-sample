9c5cc48f70d1dd611ae0b82c09b66fdd610ac622
Hendi Santika <hendisantika@yahoo.co.id>
2020-10-03 06:00:38 +0700

    #3 - Add SecurityConfiguration

---
+++ src/main/java/com/hendisantika/springdocsample/config/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package com.hendisantika.springdocsample.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springdoc-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 03/10/20
+ * Time: 05.57
+ */
+@EnableWebSecurity
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
